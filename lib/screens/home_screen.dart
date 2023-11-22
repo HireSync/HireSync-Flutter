5ebf2d65bfc45a36fe3ab0824621532290b39c6e
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-11-22 14:26:35 -0500

    update-side-bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/login_screen.dart';
 import 'package:flutter_app/screens/profile.dart';
 
 class HomePage extends StatefulWidget {
@@ -30,96 +31,10 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             SizedBox(height: 10),
-            ScheduleCard(
-              title: 'SCHEDULE',
-              events: [
-                EventCard(
-                    title: 'Entrevista con Inkafarma: 5:00pm',
-                    color: Colors.orange),
-                EventCard(
-                    title: 'Test Pendiente con Mercado Libre: 11:00am',
-                    color: Colors.purple),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            ScheduleCard(
-              title: 'Active Jobs Recruitments',
-              events: [
-                EventCard(
-                    title: 'Inkafarma',
-                    color: const Color.fromARGB(255, 255, 153, 0)),
-                EventCard(title: 'Mercado Libre', color: Colors.purple),
-              ],
-            ),
           ],
         ),
       ),
       drawer: NavigationDrawer(),
-    );
-  }
-}
-
-class ScheduleCard extends StatelessWidget {
-  final String title;
-  final List<EventCard> events;
-
-  const ScheduleCard({required this.title, required this.events});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      margin: EdgeInsets.symmetric(horizontal: 16),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              alignment: Alignment.center,
-              child: Text(
-                title,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: events,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class EventCard extends StatelessWidget {
-  final String title;
-  final Color color;
-
-  const EventCard({required this.title, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: color,
-      margin: EdgeInsets.only(bottom: 10),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          title,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
     );
   }
 }
@@ -182,20 +97,16 @@ class NavigationDrawer extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => const HomePage())),
             ),
             ListTile(
-              leading: const Icon(Icons.work_outline),
-              title: const Text('Recruitments'),
-              onTap: () {},
-            ),
-            ListTile(
               leading: const Icon(Icons.business_outlined),
               title: const Text('Organizations'),
               onTap: () {},
             ),
             ListTile(
-              leading: const Icon(Icons.settings_outlined),
-              title: const Text('Settings'),
-              onTap: () {},
-            )
+                leading: const Icon(Icons.logout),
+                title: const Text('Log Out'),
+                onTap: () => Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen())))
           ],
         ),
       );
